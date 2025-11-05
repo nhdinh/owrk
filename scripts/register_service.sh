8a564cafe7638b7a2883570ed65f72d67c052db8
Hung Dinh <hung.dnc@gmail.com>
2025-11-06 01:47:55 +0700

    Update Dockerfile for registering nginx-based services with registry

--- scripts/register_service.sh
+++ scripts/register_service.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
-
+#!/usr/bin/sh
+    
 curl -X POST http://service-registry:3000/register \
-     -H "Content-Type: application/json" \
-     -d '{
-           "name": "'${HOST_NAME}'",
-           "hostname": "'${HOST_NAME}'",
-           "port": '${HOST_PORT}',
-           "health_endpoint": "'${HEALTH_ENDPOINT}'"
-         }'
+    -H "Content-Type: application/json" \
+    -d '{
+        "name": "__HOST_NAME__",
+        "hostname": "__HOST_NAME__",
+        "port": __HOST_PORT__,
+        "health_endpoint": "__HEALTH_ENDPOINT__"
+    }'
